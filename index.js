e2812922b980544a4de232f7746fc29abe78a5c2
Nahid Muradli <124151572+nahidmrdl@users.noreply.github.com>
2024-01-08 08:13:27 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+// random password generator
+
+const result = document.getElementById("result");
+
+function generatePassword() {
+
+    const passwordLength = document.getElementById("passwordLength").value;
+    const includeLowercase = document.getElementById("includeLowercase").checked;
+    const includeUppercase = document.getElementById("includeUppercase").checked;
+    const includeNumbers = document.getElementById("includeNumbers").checked;
+    const includeSymbols = document.getElementById("includeSymbols").checked;
+
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789";
+    const symbolChars = "!@#$%^&*_+-";
+
+    let allowedChars = "";
+    let password = "";
+
+    allowedChars += includeLowercase ? lowercaseChars : "";
+    allowedChars += includeUppercase ? uppercaseChars : "";
+    allowedChars += includeNumbers ? numberChars : "";
+    allowedChars += includeSymbols ? symbolChars : "";
+
+    if (passwordLength < 3) {
+        result.textContent = "Password length must be at least 3";
+    } else if (allowedChars.length === 0) {
+        result.textContent = "At least 1 set of characters needs to be selected";
+    } else {
+        for (let i = 0; i < passwordLength; i++) {
+            const randomIndex = Math.floor(Math.random() * allowedChars.length);
+            password += allowedChars[randomIndex];
+        }
+        result.textContent = `Password: ${password}`;
+    }
+}
+
+
+
+
+
+
